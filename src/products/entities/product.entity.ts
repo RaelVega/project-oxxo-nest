32f2873911416774676409b3d0906551007ea56d
RaelVega <raelvega33@gmail.com>
2024-09-13 13:10:05 -0600

    Better get Provider methods

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -11,8 +11,8 @@ export class Product {
     price: number;
     @Column({type: "int"})
     countSeal: number;
-    //  @Column({type: "uuid"})
-    // provider: string;
-    @ManyToOne(() => Provider, (provider) => provider.products)
+    @ManyToOne(() => Provider, (provider) => provider.products, {
+        eager: true
+    })
     provider: Provider
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -32,10 +32,11 @@ export class ProductsService {
   }
 
   findByProvider(id: string) {
-    return "OK"
-    /*const productsFound = this.products.filter((product) => product.provider === id)
-    if (productsFound.length === 0) throw new NotFoundException()
-    return productsFound;*/
+    return this.productRepository.findBy({
+      provider: {
+        providerId: id,
+      }
+    })
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
