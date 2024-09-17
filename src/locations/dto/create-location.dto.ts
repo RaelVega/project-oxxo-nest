1ad6181671692e6b604ac6ecd1708f1721a5da9c
RaelVega <raelvega33@gmail.com>
2024-09-17 16:09:44 -0600

    Initial config for locations route

--- src/locations/dto/create-location.dto.ts
+++ src/locations/dto/create-location.dto.ts
@@ -1,1 +1,14 @@
-export class CreateLocationDto {}
+import { ArrayNotEmpty, IsArray, IsString, maxLength, MaxLength } from "class-validator";
+import { Location } from "../entities/location.entity";
+
+export class CreateLocationDto extends Location {
+    @IsString()
+    @MaxLength(35)
+    locationName: string;
+    @IsString()
+    @MaxLength(160)
+    locationAddress: string;
+    @IsArray()
+    @ArrayNotEmpty()
+    locationLatLng: number[];
+}
