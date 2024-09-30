2a5bdc2dd5b728c32ecec2c906a3d71f363844ea
RaelVega <raelvega33@gmail.com>
2024-09-30 15:31:33 -0600

    User relations to manager and employees

--- src/employees/entities/employee.entity.ts
+++ src/employees/entities/employee.entity.ts
@@ -1,5 +1,6 @@
-import { Location } from "src/locations/entities/location.entity";
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "src/auth/entities/user.entity";
+import { Location  } from "src/locations/entities/location.entity";
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Employee {
@@ -24,4 +25,10 @@ export class Employee {
         name: "locationId"
     })
     location: Location;
+
+    @OneToOne(() => User)
+    @JoinColumn({
+        name: "userId"
+    })
+    user: User;
 }
