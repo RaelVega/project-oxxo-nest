122ffc58d707fef2cd0e49513e132d0464efeb50
RaelVega <raelvega33@gmail.com>
2024-10-04 14:43:43 -0600

    More Swagger implementation

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,12 +1,24 @@
 import { IsEmail, IsIn, IsOptional, IsString, MinLength } from "class-validator";
 import { User } from "../entities/user.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateUserDto extends User {
+    @ApiProperty({
+        default: "user@gmail.com"
+    })
     @IsEmail()
     userEmail: string;
+
+    @ApiProperty({
+        default: "34354rr5454"
+    })
     @IsString()
     @MinLength(8)
     userPassword: string;
+
+    @ApiProperty({
+        default: "Employee"
+    })
     @IsOptional()
     @IsIn(["Admin", "Employee", "Manager"])
     userRoles: string[];
